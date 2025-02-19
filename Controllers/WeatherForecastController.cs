7e0b87159dede4113ab9ae159cf1fa3cc5d47502
srcNathan <nathanialsw@protonmail.com>
2025-02-18 21:33:20 -0500

    Added Controllers, Models folder strucutre. Split logc. Cleaner

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using MyAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var weatherForecasts = new List<WeatherForecast>();
+            for (int i = 0; i < 5; i++)
+            {
+                weatherForecasts.Add(new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(i),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                });
+            }
+            return weatherForecasts;
+        }
+    }
+}
