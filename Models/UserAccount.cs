3bd22a7bf957cd52511c160a586ff87a8390efdc
srcNathan <nathanialsw@protonmail.com>
2025-02-19 19:19:55 -0500

    Adding JWT authentication

---
+++ Models/UserAccount.cs
@@ -0,0 +1,8 @@
+namespace MyAPI.Models
+{
+    public class UserAccount
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddSingleton<JwtTokenService>();
+
+// Retrieve the JWT key from environment variables
+var jwtKey = Environment.GetEnvironmentVariable("MyAPI_JWT_KEY") ?? throw new Exception("JWT key not found in environment variables");
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
+builder.Services.AddAuthorization();
+
+
+// Build
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +41,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
